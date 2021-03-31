c783dc8defab355a7ab28efc360b83b4bf2c89a4
AbdallahMohammed <abdallah.r660@gmail.com>
2021-04-01 01:09:08 +0200

    style: 💄 export classes

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ export * from './interfaces'
 export * from './utils'
 
 export { JWT } from './jwt'
+export { JWTModule } from './jwt-module'
+export { JWTRequired } from './jwt-required'
+export { JWTOptional } from './jwt-optional'
+export { JWTServiceManager } from './jwt-service-manager'
